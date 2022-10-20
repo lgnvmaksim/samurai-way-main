7ef728667354c8ba32de3ea305fc138e0909273c
lgnvmaksim <lgnvmaksim@gmail.com>
2022-10-20 16:12:44 +0300

    dont work css (gold type text), try to refactor

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href='/profile'>Profile</a>
+                <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
             </div>
-            <div className={s.item}>
-                <a href='/Messages'>Messages</a>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to='/Messages' activeClassName={s.activeLink}>Messages</NavLink>
             </div>
             <div className={s.item}>
                 <a href='/News'>News</a>

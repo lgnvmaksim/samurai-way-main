c610d409de2672ff3c73d973da328e03abb9484f
lgnvmaksim <lgnvmaksim@gmail.com>
2022-10-21 08:52:34 +0300

    finished the desigion page of dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,22 @@
+import {state} from "../../state/state";
+import s from './Dialogs.module.css'
 
+export const Dialogs = () => {
+    let dialogs = state.dialogsPage.dialogs
+    let messages = state.dialogsPage.messages
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    {dialogs.map(d => <div>{d.name}</div>)}
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    {messages.map(m=><div>{m.message}</div>)}
+                </div>
 
-
-export const Dialogs = () =>{
-    return(
-        <div>
-            Gi
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
                 <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <NavLink to='/Messages' activeClassName={s.activeLink}>Messages</NavLink>
+                <NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink>
             </div>
             <div className={s.item}>
                 <a href='/News'>News</a>
